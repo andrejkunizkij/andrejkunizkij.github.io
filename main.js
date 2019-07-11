28c2323d766c985f4a05d6010c3b49770228d813
Andrej Kunizkij <andruxasmile7@gmail.com>
2019-07-11 22:00:46 +0200

    Add files via upload
    table_commit

--- main.js
+++ main.js
@@ -4,10 +4,23 @@ let inputName = document.querySelector('.form__input');
 let inputPass = document.querySelector('.input__pass');
 
 button.addEventListener('click', addTr);
+
 function addTr() {
-   let tr = document.createElement('tr');
-  tr.innerHTML = inputName.value;
+
+  if(inputName.value && inputPass.value !== "" ) {
+  let tr = document.createElement('tr');
+  let td1 = document.createElement('td');
+  td1.innerHTML = inputName.value;
+  let td2 = document.createElement('td');
+  td2.innerHTML = inputPass.value;
+  tr.appendChild(td1);
+  tr.appendChild(td2);
   table.appendChild(tr);
   inputName.value = "";
   inputPass.value = "";
+  table.style.display = 'table';
+}
+else {
+	alert(`Введите значение`);
+}
 }
